48a9696585d07d6771a79337cd34a79cef067913
Jordan487 <ja.arriagal@uea.edu.ec>
2025-06-08 15:24:51 -0500

    Add files via upload

---
+++ entidades/Vehiculo.java
@@ -0,0 +1,12 @@
+package entidades;
+
+public class Vehiculo {
+    
+    public void encender() {
+        System.out.println("Vehiculo encendido.");
+    }
+    
+    public void moverse() {
+        System.out.println("Vehiculo en movimiento.");
+    }
+}
